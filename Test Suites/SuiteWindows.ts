4af6e86988b98315b19d5748de7630c0c90e68bf
goncalo.f.fidalgo <goncalo.f.fidalgo@noesis.pt>
2023-10-11 20:12:04 +0100

    fix

---
+++ Test Suites/SuiteWindows.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SuiteWindows</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cacf2855-0107-4258-9827-bb6f00122b6a</testSuiteGuid>
+</TestSuiteEntity>
